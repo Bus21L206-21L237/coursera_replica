04772bd19a2a598f335bfff4db7926787eafb3fa
Bus21L206-21L237 <136835714+Bus21L206-21L237@users.noreply.github.com>
2023-11-23 01:46:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,47 +15,97 @@ function slide(){
 function toggleArrow(element) {
     element.classList.toggle('upside-down');
   }
-  function filterF() {
+
+  // Function to toggle the checked state of the radio buttons
+  
+
+  let clickCountLevel = 0;
+  let clickCountDuration = 0;
+  
+  function toggleCheckedState(radioButton, type) {
+      if (type === 'level') {
+          clickCountLevel++;
+          radioButton.checked = clickCountLevel % 2 === 1;
+      } else if (type === 'duration') {
+          clickCountDuration++;
+          radioButton.checked = clickCountDuration % 2 === 1;
+      }
+      filterF();
+  }
+  
+
+
+function filterF() {
     const levelRadios = document.getElementsByName('level');
     const durationRadios = document.getElementsByName('duration');
-  
-    let selectedLevel, selectedDuration;
-  
+
+    let selectedLevel = '';
+    let selectedDuration = '';
+
     // Get the selected level
     for (const radio of levelRadios) {
-      if (radio.checked) {
-        selectedLevel = radio.id; // Use ID as the selected value
-        break;
-      }
+        if (radio.checked) {
+            selectedLevel = radio.id; // Use ID as the selected value
+            break;
+        }
     }
-  
+
     // Get the selected duration
     for (const radio of durationRadios) {
-      if (radio.checked) {
-        selectedDuration = radio.id; // Use ID as the selected value
-        break;
-      }
+        if (radio.checked) {
+            selectedDuration = radio.id; // Use ID as the selected value
+            break;
+        }
     }
-  
+
     const cards = document.querySelectorAll('.card');
-  
+
     cards.forEach(card => {
-      const cardLevel = card.getAttribute('data-level');
-      const cardDuration = card.getAttribute('data-duration');
-  
-      // If both level and duration match the selected filters, display the card
-      if (cardLevel === selectedLevel || cardDuration === selectedDuration) {
-        card.style.display = 'block'; // Show the card
-      } else {
-        card.style.display = 'none'; // Hide the card
-      }
+        const cardLevel = card.getAttribute('data-level');
+        const cardDuration = card.getAttribute('data-duration');
+
+        // Check if the card matches the selected filters or if no filters are selected
+        if ((cardLevel==selectedLevel)||(cardDuration==selectedDuration)||((cardDuration==selectedDuration))&&(cardLevel==selectedLevel)) {
+            card.style.display = 'block'; // Show the card
+        } else {
+            card.style.display = 'none'; // Hide the card
+        }
     });
-  }
+}
+
+  
   function clearAll(){
     const cards = document.querySelectorAll('.card');
     cards.forEach(s=>{
       s.style.display='block';
     })
   }
-  
+  //here
+  function toggleSidebar() {
+    let sb = document.getElementById("sidebar");
+  let cb=document.getElementById("closebutton");
+    // Toggle the sidebar's visibility by changing its class
+    sb.classList.toggle("show-sidebar");
+    if (sb.classList.contains("show-sidebar")) {
+      cb.style.left="-80px";
+      cb.innerHTML = '&#10005'; // Change to close (cross) icon
+     
+    } 
+    else {
+      cb.innerHTML = '&#9776'; // Change back to hamburger icon
+      cb.style.left="-240px";
+    }
+  }
+  function displayFilter(){
+    console.log("called");
+    let content=document.getElementById("filterdiv");
+    content.classList.toggle("sidebar");
 
+  }
+  function closeFilter(){
+    let closingcontent=document.getElementById("filterdiv");
+    closingcontent.classList.toggle("sidebar");
+   
+  }
+  //here
+  
